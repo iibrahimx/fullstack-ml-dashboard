0f00d673722ad7424ad7fe20ae3f77f8498d1324
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-03-04 19:18:22 +0100

    chore: setup react and tailwind in frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello Tailwind</h1>
+      </div>
+      <p className="text-xl font-medium">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
